b318f97697bf62f5957dfdfbb749d27a8d38c7a3
Adrien Baffioni <117181235+AdrienB23@users.noreply.github.com>
2023-06-15 22:08:57 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -35,45 +35,22 @@ include("data/pages/traduction.inc.php");
 
     <!-- Image -->
     <div class="accueil-content">
-        <span id="title-behind-img"><?php echo $translations["Cordoue"];?></span>
+        <span id="title-behind-img"><?php echo $translations["Cordoue"] ?></span>
         <img src="data/images/foreground3.webp" alt="foreground" id="foreground" />
     </div>
     <!-- Contenu -->
     <section class="page-content">
         <section class="content">
-            <h1><?php echo $translations["histoire"];?></h1>
+            <h1><?php echo $translations["histoire"] ?></h1>
             <br>
-            <p>
-                Cordoue est une ville située en Andalousie (région du sud de l'Espagne), et la capitale de la province
-                du même
-                nom.
-                C'est l'une des villes les plus peuplées d'Andalousie avec 326 039 habitants en 2020.
-                La ville est connue pour son riche patrimoine architectural et culturel qui évoquent les différentes
-                phases de
-                son histoire. En effet, la ville a été érigée par les romains et dirigée ensuite par les musulmans.
-            </p>
-            <p>
-                Cordoue a été fondée au IIe siècle av J.C. par les romains, la ville acquit une grande importance à
-                l'époque d'Auguste. Au VIIIe siècle, elle devint la capitale de l'émirat dépendant de Damas. C'est
-                ensuite en 929, que Abd al-Rahman III établit la ville comme siège du califat. Pendant cette période
-                (qui est qualifiée comme la période glorieuse de Cordoue) commença la construction de 300 mosquées et
-                d'innombrables palais et édifices publics. La ville rivalisait avec les splendeurs de Constantinople,
-                Damas et Bagdad. Au XIIIe siècle, sous Ferdinand III fut construite la Grande Mosquée de Cordoue, en
-                plus de l'Alcazar de los Reyes Cristianos ou la tour de la Calahorra.
-                La ville est connue pour son patrimoine associant grand nombre de valeurs artistiques d'Orient et
-                d'Occident et incluant des éléments jusque-là inconnus dans l'architecture religieuse islamique.
-            </p>
-            <p>
-                Depuis le paléolithique, la province de Cordoue est marquée par la présence de l’homme. Tartessiens et
-                Orétans se disputèrent ces terres et l’exploitation de ses mines de fer, de plomb et de cuivre.
-            </p>
-            <p><a href="data/pages/histoire.php">En savoir plus</a></p>
+            <p><?php echo $translations["histoire_p"] ?></p>
+            <p><a href="data/pages/histoire.php"><?php echo $translations["savoir_plus"] ?></a></p>
             <br>
         </section>
     </section>
     <section class="page-content">
         <section class="content">
-            <h1>Actualité</h1>
+            <h1><?php echo $translations["actualite"] ?></h1>
             <br>
             <div class="fil_twitter">
                 <a class="twitter-timeline" data-width="500" data-height="500" data-theme="dark"
@@ -81,16 +58,7 @@ include("data/pages/traduction.inc.php");
                 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
             </div>
             <br>
-            <p>
-                Jusqu'au 29 octobre, plusieurs patios de la vieille ville accueillent, à l'occasion du festival
-                international
-                Flora, les œuvres monumentales de huit grands artistes floraux.
-                Cordoue, la plus septentrionale des grandes cités andalouses, sera pendant les dix jours qui viennent la
-                capitale mondiale des fleurs. Jusqu'au 29 octobre, en effet, huit patios de la vieille ville, ces
-                splendides cours intérieures dont l'origine remonte aux atriums romains, accueilleront, dans le cadre du
-                festival international Flora, des œuvres monumentales réalisées par huit des plus grands artistes
-                floraux de la planète.
-            </p>
+            <p><?php echo $translations["actualite_p"] ?></p>
             <!-- Diapo -->
             <br>
             <div id="slideshow">
