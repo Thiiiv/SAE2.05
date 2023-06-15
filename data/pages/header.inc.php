29b1f090c8f1f5bb5668fde00232f75b84dab52e
Adrien Baffioni <117181235+AdrienB23@users.noreply.github.com>
2023-06-15 15:20:58 +0200

    Add files via upload

--- data/pages/header.inc.php
+++ data/pages/header.inc.php
@@ -1,8 +1,8 @@
 <?php
 if (isset($_SESSION['location'])) {
-    $images = "";
-    $liens = "";
-    $index = "";
+    $images = "../../images/";
+    $liens = "../";
+    $index = "../../../index.php";
     switch ($_SESSION['location']) {
         case "root":
             $images = "data/images/";
@@ -14,50 +14,51 @@ if (isset($_SESSION['location'])) {
             $liens = "";
             $index = "../../index.php";
             break;
-        case "data/pages/monuments/":
-            $images = "../../images/";
-            $liens = "../";
-            $index = "../../../index.php";
-            break;
-        case "data/pages/histoire/":
-            $images = "../../images/";
-            $liens = "../";
-            $index = "../../../index.php";
-            break;
     }
 }
-
-    echo '
-<header>
-    <div class="menu">
-    <div class="accueil">
-    <a href="' . $index . '" class="lien">
-        <img src="' . $images . 'logo.png" alt="logo"/>
-    </a>
-    </div>
-    <div class="liens">
-    ';
-
-    if (isset($_SESSION['statut']) && $_SESSION['statut'] == 1){
-        echo '<a href="' . $liens . 'gestionEvenements.php" class="lien">Gestion Evenements</a>';
-        echo '<div style="flex:10;margin-right:20vw;"></div>';
+$jsonData = file_get_contents("$liens../json/fr.json");
+$translations = json_decode($jsonData, true);
+if (isset($_GET["langue"])) {
+    $langue = $_GET["langue"];
+    if ($langue == "en") {
+        $jsonData = file_get_contents("$liens../json/ang.json");
+        $translations = json_decode($jsonData, true);
     }
-    //print_r($_SESSION);
-    
-    echo '<a href="' . $liens . 'histoire.php" class="lien">Histoire</a>
-        <a href="' . $liens . 'monuments.php" class="lien">Monuments</a>
-        <a href="' . $liens . 'evenements.php"  class="lien">Evenements</a>
-        <a href="' . $liens . 'carte.php"  class="lien">Carte</a>
-        <div class="lien langues">
-        <a href="' . $index . '">Langues</a>
-            <div class="langue">
-                <a href="' . $index . '">Français</a>
-                <a href="' . $index . '">English</a>
-            </div>
+}
+if (!isset($_GET["langue"])) {
+    $langue = "fr";
+}
+?>
+
+<header>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <div class="menu">
+        <div class="accueil">
+            <a href="<?php echo $index . '?langue=' . $langue;?>" class="lien">
+                <img src="<?php echo $images.'logo.png';?>" alt="logo"/>
+            </a>
         </div>
-        <a href="' . $liens . 'connexion.php" class="lien"><img src="' . $images . 'connect.png" alt="connexion" id="connexion"/></a>
-        <button class="lien"><img src="' . $images . 'recherche.png" alt="Recherche"></button>
+        <div class="liens">
+            <?php
+            if (isset($_SESSION['statut']) && $_SESSION['statut'] == 1){
+                echo '<a href="'.$liens.'gestionEvenements.php?langue=' . $langue . '" class="lien">Gestion Evenements</a>';
+                echo '<div style="flex:10;margin-right:20vw;"></div>';
+            }
+            ?>
+            
+            <a href="<?php echo $liens.'histoire.php?langue=' . $langue;?>" class="lien"><?php echo $translations["histoire"];?></a>
+            <a href="<?php echo $liens.'monuments.php?langue=' . $langue;?>" class="lien"><?php echo $translations["monuments"];?></a>
+            <a href="<?php echo $liens.'evenements.php?langue=' . $langue;?>"  class="lien"><?php echo $translations["evenements"];?></a>
+            <a href="<?php echo $liens.'parcours.php?langue=' . $langue;?>"  class="lien"><?php echo $translations["parcours"];?></a>
+            <div class="langues">
+                <a href="?langue=fr" class="lien"><?php echo $translations["langue"];?></a>
+                    <div class="langue">
+                        <a href="?langue=fr" class="lien"><img src="../images/fr.png" alt="FR"></a>
+                        <a href="?langue=en" class="lien"><img src="../images/eng.png" alt="EN"></a>
+                    </div>
+            </div>
+            <a href="<?php echo $liens.'connexion.php?langue=' . $langue;?>" class="lien"><img src="<?php echo $images.'connect.png';?>" alt="connexion" id="connexion"/></a>
         </div>
     </div>
-</header>';
-?>
+</header>
